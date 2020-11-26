192ea38ccc81efcedf61eb4d577874c8e9c2cfca
Ayush Khare <45105548+mizumugen@users.noreply.github.com>
2020-11-26 15:55:00 +0530

    Moves working for P1, P2, H3

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ class game{
         legal_moves["H1"] = {"F", "L", "R", "B"};
         legal_moves["H2"] = {"FL", "FR", "BR", "BL"};
         legal_moves["H3"] = {"FL", "FR", "BL", "BR", "RF", "RB", "LF", "LB"};
+        
         move_matrix[{"P1","F"}] = {1, 0};
         move_matrix[{"P1","B"}] = {-1, 0};
         move_matrix[{"P1","R"}] = {0, 1};
@@ -59,10 +60,19 @@ class game{
             cout<<endl;
         }
     }
-    void markgrid(string character, vector<int> new_pos, vector<int> old_pos)
+    void markgrid(string character, vector<int> new_pos, vector<int> old_pos, int player, int char_no)
     {
+        if(grid[new_pos[0]][new_pos[1]]!="-")
+        {
+            if(character[0]=='A')
+                p2--;
+            else 
+                p1--;
+        }
         grid[new_pos[0]][new_pos[1]] = character;
         grid[old_pos[0]][old_pos[1]] = "-";
+        pos[character[0]=='A'?0:1][char_no][0] = new_pos[0];
+        pos[character[0]=='A'?0:1][char_no][1] = new_pos[1];
         return;
     }
 
@@ -73,27 +83,27 @@ class game{
         cin>>s;
         pos[player-1][0][1] = s[1]-'1';
         pos[player-1][0][0] = (player==1?0:4);
-        player == 1 ? markgrid("A-P1", pos[player-1][0], {2,2}): markgrid("B-P1", pos[player-1][0], {2,2});
+        player == 1 ? markgrid("A-P1", pos[player-1][0], {2,2}, player, 0): markgrid("B-P1", pos[player-1][0], {2,2}, player, 0);
         cout<<"Enter the position of Pawn 2: ";
         cin>>s;
         pos[player-1][1][1] = s[1]-'1';
         pos[player-1][1][0] = (player==1?0:4);
-        player == 1 ? markgrid("A-P2", pos[player-1][1], {2,2}): markgrid("B-P2", pos[player-1][1], {2,2});
+        player == 1 ? markgrid("A-P2", pos[player-1][1], {2,2}, player, 1): markgrid("B-P2", pos[player-1][1], {2,2}, player, 1);
         cout<<"Enter the position of Hero 1: ";
         cin>>s;
         pos[player-1][2][1] = s[1]-'1';
         pos[player-1][2][0] = (player==1?0:4);
-        player == 1 ? markgrid("A-H1", pos[player-1][2], {2,2}): markgrid("B-H1", pos[player-1][2], {2,2});
+        player == 1 ? markgrid("A-H1", pos[player-1][2], {2,2}, player, 2): markgrid("B-H1", pos[player-1][2], {2,2}, player, 2);
         cout<<"Enter the position of Hero 2: ";
         cin>>s;
         pos[player-1][3][1] = s[1]-'1';
         pos[player-1][3][0] = (player==1?0:4);
-        player == 1 ? markgrid("A-H2", pos[player-1][3], {2,2}): markgrid("B-H2", pos[player-1][3], {2,2});
+        player == 1 ? markgrid("A-H2", pos[player-1][3], {2,2}, player, 3): markgrid("B-H2", pos[player-1][3], {2,2}, player, 3);
         cout<<"Enter the position of Hero 3: ";
         cin>>s;
         pos[player-1][4][1] = s[1]-'1';
         pos[player-1][4][0] = (player==1?0:4);
-        player == 1 ? markgrid("A-H3", pos[player-1][4], {2,2}): markgrid("B-H3", pos[player-1][4], {2,2});
+        player == 1 ? markgrid("A-H3", pos[player-1][4], {2,2}, player, 4): markgrid("B-H3", pos[player-1][4], {2,2}, player, 4);
         display();
     }
     
@@ -116,7 +126,7 @@ class game{
             {   
                 if(position==legal_moves[character][i])
                 {
-                    
+                    makeMove(character, position, player);
                     return;
                 }
             }
@@ -128,40 +138,57 @@ class game{
             }
         }
 
-        bool nextMoveOnBoard(int player, int char_no, string position, string character)
-        {
-            if((pos[player-1][char_no][0]+move_matrix[{character, position}][0]>=0)
-                &&(pos[player-1][char_no][1]+move_matrix[{character, position}][1]>=0)
-                &&(pos[player-1][char_no][0]+move_matrix[{character, position}][0]<5)
-                &&(pos[player-1][char_no][1]+move_matrix[{character, position}][0]<5))
+    }
+
+    bool nextMoveOnBoard(int player, int char_no, string position, string character)
+    {
+        if((pos[player-1][char_no][0]+move_matrix[{character, position}][0]>=0)
+            &&(pos[player-1][char_no][1]+move_matrix[{character, position}][1]>=0)
+            &&(pos[player-1][char_no][0]+move_matrix[{character, position}][0]<5)
+            &&(pos[player-1][char_no][1]+move_matrix[{character, position}][1]<5))
                 return true;
-            else
-                return false;
-        }
+        return false;
+    }
 
-        void makemove(string character, string position, int player)
+    void makeMove(string character, string position, int player)
+    {
+        int char_no;
+        if(character=="P1")
+            char_no = 0;
+        else if(character=="P2")
+            char_no = 1;
+        else if(character=="H1")
+            char_no = 2;
+        else if(character=="H2")
+            char_no = 3;
+        else if(character=="H3")
+            char_no = 4;
+        if(!nextMoveOnBoard)
         {
-            int char_no;
-            if(character=="P1")
-                char_no = 0;
-            else if(character=="P2")
-                char_no = 1;
-            else if(character=="H1")
-                char_no = 2;
-            else if(character=="H2")
-                char_no = 3;
-            else if(character=="H3")
-                char_no = 4;
-            if(!nextMoveOnBoard)
-            {
-                cout<<"Move Lands you out of the board. Thus, Invalid Move, Retry!" ;
-                chance(player);
-                return;  
-            }
-
-            
+            cout<<"Move Lands you out of the board. Thus, Invalid Move, Retry!" ;
+            chance(player);
+            return;  
         }
-        
+        for(int i=0; i<5; i++)
+        {
+            if((pos[player-1][char_no][0]+move_matrix[{character, position}][0]==pos[player-1][i][0])
+            &&(pos[player-1][char_no][1]+move_matrix[{character, position}][1]==pos[player-1][i][0]))
+            {
+                cout<<"Move Lands you on your another chacacter. Thus, Invalid Move, Retry!" ;
+                chance(player);
+                return;
+            }
+        }
+        if(char_no==0||char_no ==1||char_no==4)
+        {
+            markgrid(player==1? "A"+character:"B"+character,{pos[player-1][char_no][0]+move_matrix[{character, position}][0], 
+                pos[player-1][char_no][1]+move_matrix[{character, position}][1]}, pos[player-1][char_no], player, char_no);
+                display();
+                return ;
+        }
+
+
+      
     }
 
 
